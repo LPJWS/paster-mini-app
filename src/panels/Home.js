9c4ec260accecc48cc6dea2ac261d986e41e2886
Daniil Volkov <lpjakewolfskin@gmail.com>
2021-11-08 18:37:25 +0300

    add pics

--- src/panels/Home.js
+++ src/panels/Home.js
@@ -34,6 +34,8 @@ const Home = ({ id, go, fetchedUser, pastePreloaded }) => {
 
 	async function getPaste() {
 		setPopout(<ScreenSpinner/>)
+		// const params = window.location.search.slice(1);
+		// console.log(params);
 		fetch('https://lpjakewolfskin.ru/api/v1/paste/get/unrelated/?vk_id=' + fetchedUser.id)
 		  .then(response => {
 				if (!response.ok) {
@@ -114,8 +116,16 @@ const Home = ({ id, go, fetchedUser, pastePreloaded }) => {
 					<Group header={<Header mode="secondary">Данную пасту оценили {paste.cnt} раз, рейтинг - {paste.rating}</Header>}>
 						<Div>
 							<Card size="l" mode="shadow" style={{marginBottom:'1rem'}}>
-								<Div style={{padding: '.5rem'}}><p>{paste.clear_text}</p></Div>
+								<Div style={{padding: '.5rem', margin: 'auto'}}>
+									<p>{paste.clear_text}</p>
+									<img style={{width: "100%", height: "100%"}} src={paste.pic_link} />
+								</Div>
+								
 							</Card>
+							
+						</Div>
+						<Div style={{margin: 'auto'}}>
+							
 						</Div>
 						<Group header={<Header mode="secondary">Оцените пасту</Header>}>
 							<Div style={{display: 'flex'}}>
