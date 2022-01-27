68bf48b6465bde698b36047d48c5412f574309ce
Daniil Volkov <lpjakewolfskin@gmail.com>
2022-01-27 04:12:51 +0300

    add suggests

--- src/panels/Moderator.js
+++ src/panels/Moderator.js
@@ -12,12 +12,14 @@ import { Icon28CheckCircleOutline } from '@vkontakte/icons';
 const Moderator = props => {
 	const [pastes, setPastes] = useState([])
 	const [ paste, setPaste] = useState(null);
+	const [ suggest, setSuggest] = useState(null);
 	const [you, setYou] = useState(null);
 	const [ snackbar, setSnackbar] = useState(null);
 	const [popout, setPopout] = useState(null);
 	const [activeTab, setActiveTab] = useState('tags');
 	const [tagsList, setTagsList] = useState([]);
 	const [selectedTags, setSelectedTags] = React.useState([]);
+	const [selectedTagsSuggest, setSelectedTagsSuggest] = React.useState([]);
 
 	function getYou() {
 		setPopout(<ScreenSpinner/>)
@@ -78,6 +80,24 @@ const Moderator = props => {
 		  })
 	}
 
+	async function getSuggest() {
+		setPopout(<ScreenSpinner/>)
+		fetch('https://lpjakewolfskin.ru/api/v1/wall/suggests/?vk_id=' + props.fetchedUser.id)
+		  .then(response => {
+				if (!response.ok) {
+					throw new Error(response.statusText)
+				}
+				return response.json()
+				}).catch(err=>{
+				console.log(err)
+			})
+		  .then(paste => {
+			setPopout(null)
+			setSuggest(paste)
+			setSelectedTagsSuggest([])
+		  })
+	}
+
 	function deletePaste() {
 		fetch('https://lpjakewolfskin.ru/api/v1/paste/delete/', {
 			method: 'DELETE',
@@ -132,10 +152,65 @@ const Moderator = props => {
 		  })
 	}
 
+	function postSuggest() {
+		fetch('https://lpjakewolfskin.ru/api/v1/wall/suggests/post/', {
+			method: 'POST',
+			headers: { 'Content-Type': 'application/json' },
+			body: JSON.stringify({ id: suggest.item.id, vk_id: props.fetchedUser.id, tags: selectedTagsSuggest })
+		})
+		  .then(response => {
+				if (!response.ok) {
+					throw new Error(response.statusText)
+				}
+				return response.json()
+				}).catch(err=>{
+				console.log(err)
+			})
+		  .then(() => {
+			setSnackbar(
+				<Snackbar
+					onClose={() => setSnackbar(null)}
+					after={<IconButton onClick={() => setSnackbar(null)}><Icon16Clear /></IconButton>}
+				>
+					Предложка опубликована
+				</Snackbar>
+			)
+			getSuggest()
+		  })
+	}
+
+	function deleteSuggest() {
+		fetch('https://lpjakewolfskin.ru/api/v1/wall/suggests/deny/', {
+			method: 'POST',
+			headers: { 'Content-Type': 'application/json' },
+			body: JSON.stringify({ id: suggest.item.id, vk_id: props.fetchedUser.id })
+		})
+		  .then(response => {
+				if (!response.ok) {
+					throw new Error(response.statusText)
+				}
+				return response.json()
+				}).catch(err=>{
+				console.log(err)
+			})
+		  .then(() => {
+			setSnackbar(
+				<Snackbar
+					onClose={() => setSnackbar(null)}
+					after={<IconButton onClick={() => setSnackbar(null)}><Icon16Clear /></IconButton>}
+				>
+					Предложка удалена
+				</Snackbar>
+			)
+			getSuggest()
+		  })
+	}
+
 	useEffect(() => {
 		getYou()
 		getTags()
 		getPaste()
+		getSuggest()
 	  }, [])
 
 	return (
@@ -144,20 +219,20 @@ const Moderator = props => {
 				<PanelHeader>
 					Модерация
 				</PanelHeader>
-				{/* <Tabs>
+				<Tabs>
 					<TabsItem
 						onClick={() => setActiveTab('tags')}
 						selected={activeTab === 'tags'}
 					>
-						Пасты
+						Разметка паст
 					</TabsItem>
 					<TabsItem
-						onClick={() => setActiveTab('member')}
-						selected={activeTab === 'member'}
+						onClick={() => setActiveTab('suggests')}
+						selected={activeTab === 'suggests'}
 					>
-						Пользователи
+						Предложка
 					</TabsItem>
-              	</Tabs> */}
+              	</Tabs>
 				{activeTab === 'tags' && <Group header={<Header mode="secondary">Разметка паст</Header>}>
 					<Div>
 					{paste && 
@@ -168,7 +243,7 @@ const Moderator = props => {
 								<p>Рейтинг: {paste.rating}</p>
 								<p>Прислал: {paste.sender ? paste.sender.name : 'Автопарсинг'}</p>
 								<p>Опубликована: {paste.link_self ? paste.link_self : 'Нет'}</p>
-
+								<p>Неразмеченных паст: {paste.untaged ? paste.untaged : 'Нет'}</p>
 							</Div>
 							<Div>
 								<Card size="l" mode="shadow" style={{marginBottom:'1rem'}}>
@@ -217,7 +292,7 @@ const Moderator = props => {
 										() => {getPaste()}} before={<Icon28ViewOutline/>}>
 										Получить другую пасту
 									</Button>
-									<Button stretched size="s" mode="positive" onClick={
+									<Button stretched size="s" disabled={selectedTags.length == 0} mode="positive" onClick={
 										() => {tagPaste()}} before={<Icon28CheckCircleOutline/>}>
 										Отправить теги
 									</Button>
@@ -226,6 +301,70 @@ const Moderator = props => {
 						</Group>
 					}
 					
+					</Div>
+				</Group>}
+				{activeTab === 'suggests' && <Group header={<Header mode="secondary">Предложка</Header>}>
+					<Div>
+					{suggest && suggest.count != 0 &&
+						<Group>
+							<Div style={{color: 'grey', fontSize: 10, textAlign: 'center'}}>
+								<p>Прислал: {suggest.member.name}</p>
+								<p>Паст в предложке: {suggest.count}</p>
+							</Div>
+							<Div>
+								<Card size="l" mode="shadow" style={{marginBottom:'1rem'}}>
+									<Div style={{padding: '.5rem', margin: 'auto'}}>
+										<p>{suggest.item.text}</p>
+										{suggest.photo &&
+											<img style={{width: "100%", height: "100%"}} src={suggest.photo} />
+										}
+									</Div>
+								</Card>
+								
+							</Div>
+							<Div style={{margin: 'auto'}}>
+								
+							</Div>
+							<Group header={<Header mode="secondary">Оцените пасту</Header>}>
+								<FormItem top="Выберите теги (желательно не больше 2-х)">
+								{
+									tagsList.map( (tag, index) => {
+										return (
+											<Checkbox
+												key={tag.value}
+												id={tag.value}
+												checked={selectedTagsSuggest.includes(tag.value)}
+												onChange={e => {
+														if (!selectedTagsSuggest.includes(parseInt(e.target.id))) {
+															setSelectedTagsSuggest([...selectedTagsSuggest, parseInt(e.target.id)])
+														}
+														else {
+															setSelectedTagsSuggest(selectedTagsSuggest.filter((i, _) => i != parseInt(e.target.id)))
+														}
+												}}
+											>
+												{tag.label}
+											</Checkbox>
+										)
+									})
+								}
+								</FormItem>
+								<Div style={{display: 'flex'}}>
+									<Button stretched size="s" style={{ marginRight: '.1rem' }} mode="negative" onClick={
+										() => {deleteSuggest()}} before={<Icon28DeleteOutline/>}>
+										Удалить
+									</Button>
+									<Button stretched size="s" disabled={selectedTagsSuggest.length == 0} mode="positive" onClick={
+										() => {postSuggest()}} before={<Icon28CheckCircleOutline/>}>
+										Опубликовать
+									</Button>
+								</Div>
+							</Group>
+						</Group>
+					}
+					{suggest && suggest.count == 0 &&
+						<Div>Нету нихуя</Div>
+					}
 					</Div>
 				</Group>}
 				{snackbar}
